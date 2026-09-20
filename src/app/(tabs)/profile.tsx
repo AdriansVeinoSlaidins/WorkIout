31413096e9a2d0a5328e43a7a9501d78cce6d477
AdriansVeinoSlaidins <slaidinsa@gmail.com>
2026-09-20 13:36:00 +0300

    Add new button

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,11 +1,21 @@
-import { globalStyles } from "@/styles/global";
-import { Text, ScrollView } from "react-native";
+import Button from "@/components/Button";
+import { colors, globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
 
 
 export default function () {
     return (
         <ScrollView style={globalStyles.container}>
             <Text style={globalStyles.title}>Profile</Text>
+            <Text style={globalStyles.title}>Workout</Text>
+                        <Button 
+                            labeltext="Test"
+                            color={colors.primary}
+                            OnPress={() => {
+                                console.log("Test Pressed");
+                            }}
+                            />
+
         </ScrollView>
     )
 }
